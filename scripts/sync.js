e8907a09d6640bd5ac70de787be47ec954601c95
Bhushan Goel <bhushan.goel@ihsmarkit.com>
2018-12-07 18:45:43 +0530

    changing layout

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,24 +1,33 @@
-averageBlock = (numbers) => {
-    console.log('numbers called :: ', numbers)
-    let startTime = new Date().getTime();
-    let len = numbers,
-        sum = 0,
-        i;
+(() => {
+    document.getElementById('valueBlock').value = 10000;
+    averageBlock = () => {
+        try {
+            var numbers = document.getElementById('valueBlock').value;
+        }
+        catch (e) {
+            var numbers = 10000;
+        }
+        let startTime = new Date().getTime();
+        let len = numbers,
+            sum = 0,
+            i;
 
-    if (len === 0) {
-        return 0;
+        if (len === 0) {
+            return 0;
+        }
+
+        for (i = 0; i < len; i++) {
+            console.log('i :: ', i)
+            sum += i;
+        }
+
+        let endTime = new Date().getTime();
+        document.getElementById('timeBlock').innerHTML = `${(endTime - startTime) / 1000} seconds`;
+        alert('Average - ', sum / len);
+    };
+
+    helloBlock = () => {
+        alert("Hello World !!");
     }
+})();
 
-    for (i = 0; i < len; i++) {
-        console.log('i :: ', i)
-        sum += i;
-    }
-
-    let endTime = new Date().getTime();
-    document.getElementById('timeBlock').innerHTML = `${(endTime - startTime) / 1000} seconds`;
-    alert('Average - ', sum / len);
-}
-
-helloBlock = () => {
-    alert("Hello World !!");
-}
